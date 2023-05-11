f5e284b3e157f3460fa21759bb4045775179c297
Kania <kania.praca@wp.pl>
2023-05-11 12:11:13 +0200

    Create repository for Task entity

---
+++ src/main/java/com/github/christianj98/model/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.github.christianj98.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+}
