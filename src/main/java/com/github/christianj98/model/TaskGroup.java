15d4476c33802dd9747efa31d2e56a7c1e3a68a8
Kania <kania.praca@wp.pl>
2023-05-14 13:40:48 +0200

    Create new table 'task_groups' and corresponding entity

---
+++ src/main/java/com/github/christianj98/model/TaskGroup.java
@@ -0,0 +1,58 @@
+package com.github.christianj98.model;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "task_groups")
+public class TaskGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "Task group's description must not be empty")
+    private String description;
+
+    private boolean done;
+
+    @Embedded
+    private Audit audit = new Audit();
+
+    public TaskGroup() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public void updateFrom(final TaskGroup source) {
+        description = source.getDescription();
+        done = source.isDone();
+    }
+}
---
+++ src/main/resources/db/migration/V5__init_task_groups_table.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS task_groups;
+
+CREATE TABLE task_groups
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    description VARCHAR(100) NOT NULL,
+    done        BIT
+);
+
+ALTER TABLE TASKS ADD COLUMN task_group_id INT NULL;
+ALTER TABLE TASKS ADD FOREIGN KEY (task_group_id) REFERENCES task_groups (id);
