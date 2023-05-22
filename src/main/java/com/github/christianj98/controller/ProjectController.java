8cf15cc9a6f9c2c6cb2f2b31e4d0cd57ccfee538
vanvi <kania.praca@wp.pl>
2023-05-22 17:12:33 +0200

    Extend ProjectController

--- src/main/java/com/github/christianj98/controller/ProjectController.java
+++ src/main/java/com/github/christianj98/controller/ProjectController.java
@@ -1,10 +1,17 @@
 package com.github.christianj98.controller;
 
+import com.github.christianj98.logic.ProjectService;
+import com.github.christianj98.model.Project;
+import com.github.christianj98.model.ProjectStep;
 import com.github.christianj98.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Project controller is responsible for returning template
@@ -12,6 +19,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+    private final ProjectService projectService;
+
+    public ProjectController(final ProjectService projectService) {
+        this.projectService = projectService;
+    }
 
     @GetMapping
     public String showProjects(Model model) {
@@ -19,6 +31,26 @@ public class ProjectController {
 
         // Spring tries to find template with name projects.html, if it will find then the view will be rendered in the browser
         return "projects";
+    }
+
+    @PostMapping
+    public String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model) {
+        projectService.createProject(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Dodano project!");
+        return "projects";
+    }
+
+    @PostMapping(params = "addStep")
+    // @ModelAttribute -> attribute is from model "project"
+    public String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
+        current.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
+    @ModelAttribute("projects")
+    public List<Project> getProjects() {
+        return projectService.readAll();
     }
 
 }
