5b1777c13fad08873b88cd92fe7eceb67ec150d5
vanvi <kania.praca@wp.pl>
2023-05-22 18:59:52 +0200

    Add buildingResult and two css classes

--- src/main/java/com/github/christianj98/controller/ProjectController.java
+++ src/main/java/com/github/christianj98/controller/ProjectController.java
@@ -6,11 +6,13 @@ import com.github.christianj98.model.ProjectStep;
 import com.github.christianj98.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -34,7 +36,12 @@ public class ProjectController {
     }
 
     @PostMapping
-    public String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model) {
+    public String addProject(@ModelAttribute("project")  @Valid ProjectWriteModel current,
+                             BindingResult bindingResult,
+                             Model model) {
+        if (bindingResult.hasErrors()) {
+            return "projects";
+        }
         projectService.createProject(current);
         model.addAttribute("project", new ProjectWriteModel());
         model.addAttribute("message", "Dodano project!");
