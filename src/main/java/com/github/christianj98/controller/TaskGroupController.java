6d017d016e53f14f8cfe0da11ddfa1a39c8809e8
vanvi <kania.praca@wp.pl>
2023-05-23 22:14:02 +0200

    Create view for groups and adjust TaskGroupController

--- src/main/java/com/github/christianj98/controller/TaskGroupController.java
+++ src/main/java/com/github/christianj98/controller/TaskGroupController.java
@@ -6,18 +6,24 @@ import com.github.christianj98.model.TaskGroupRepository;
 import com.github.christianj98.model.TaskRepository;
 import com.github.christianj98.model.projection.GroupReadModel;
 import com.github.christianj98.model.projection.GroupTaskReadModel;
+import com.github.christianj98.model.projection.GroupTaskWriteModel;
 import com.github.christianj98.model.projection.GroupWriteModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -25,7 +31,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
 @RequestMapping("/groups")
 public class TaskGroupController {
     private static final Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
@@ -43,7 +49,36 @@ public class TaskGroupController {
         this.taskRepository = taskRepository;
     }
 
-    @PostMapping
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    public String showGroups(Model model) {
+        model.addAttribute("group", new GroupWriteModel());
+        return "groups";
+    }
+
+
+    @PostMapping(produces = MediaType.TEXT_HTML_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String addGroup(@ModelAttribute("group") @Valid GroupWriteModel current,
+                           BindingResult bindingResult,
+                           Model model) {
+        if (bindingResult.hasErrors()) {
+            return "groups";
+        }
+        taskGroupService.createGroup(current);
+        model.addAttribute("group", new GroupWriteModel());
+        model.addAttribute("groups", getGroups());
+        model.addAttribute("message", "Dodano grupę!");
+        return "groups";
+    }
+
+    @PostMapping(params = "addTask", produces = MediaType.TEXT_HTML_VALUE)
+    // @ModelAttribute -> attribute is from model "project"
+    public String addGroupTask(@ModelAttribute("group") GroupWriteModel current) {
+        current.getTasks().add(new GroupTaskWriteModel());
+        return "groups";
+    }
+
+    @ResponseBody
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GroupReadModel> createGroup(@RequestBody @Valid final GroupWriteModel source) {
         logger.info("create group");
         final GroupReadModel createdGroup = taskGroupService.createGroup(source);
@@ -56,12 +91,14 @@ public class TaskGroupController {
         return ResponseEntity.created(URI.create(uri)).body(createdGroup);
     }
 
-    @GetMapping
+    @ResponseBody
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<GroupReadModel>> readAllGroups() {
         logger.info("read all groups");
         return ResponseEntity.ok(taskGroupService.readAll());
     }
 
+    @ResponseBody
     @PatchMapping("/{groupId}")
     public ResponseEntity<?> toggleGroup(@PathVariable final int groupId) {
         logger.info("toggle group");
@@ -72,11 +109,17 @@ public class TaskGroupController {
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/{id}/tasks")
+    @ResponseBody
+    @GetMapping(value = "/{id}/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<GroupTaskReadModel>> readAllTaskWithinGroup(@PathVariable final int id) {
         return ResponseEntity.ok(taskRepository.findByGroup_Id(id).stream()
                 .map(GroupTaskReadModel::new)
                 .collect(Collectors.toList()));
+    }
+
+    @ModelAttribute("groups")
+    private List<GroupReadModel> getGroups() {
+        return taskGroupService.readAll();
     }
 
     /*
--- src/main/java/com/github/christianj98/model/projection/GroupTaskWriteModel.java
+++ src/main/java/com/github/christianj98/model/projection/GroupTaskWriteModel.java
@@ -1,12 +1,16 @@
 package com.github.christianj98.model.projection;
 
 import com.github.christianj98.model.Task;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 public class GroupTaskWriteModel {
+    @NotBlank(message = "Task's description must not be empty!")
     private String description;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime deadline;
 
     public String getDescription() {
