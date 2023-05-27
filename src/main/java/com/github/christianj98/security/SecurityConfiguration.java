c64354e4cab3682a03ec244a7abeddb2eb567b55
vanvi <kania.praca@wp.pl>
2023-05-27 13:15:43 +0200

    Add configure method and global settings for Spring Security

--- src/main/java/com/github/christianj98/security/SecurityConfiguration.java
+++ src/main/java/com/github/christianj98/security/SecurityConfiguration.java
@@ -2,8 +2,13 @@ package com.github.christianj98.security;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -16,8 +21,29 @@ public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter 
         return new KeycloakSpringBootConfigResolver();
     }
 
+    @Autowired
+    void configureGlobal(AuthenticationManagerBuilder authBuilder) {
+        var authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setConvertToUpperCase(true);
+        authorityMapper.setPrefix("ROLE_");
+
+        KeycloakAuthenticationProvider keycloakProvider = keycloakAuthenticationProvider();
+        keycloakProvider.setGrantedAuthoritiesMapper(authorityMapper);
+        authBuilder.authenticationProvider(keycloakProvider);
+    }
+
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/info/*")
+                .hasRole("USER")
+                .anyRequest()
+                .permitAll();
     }
 }
